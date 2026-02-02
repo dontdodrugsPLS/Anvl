a4b48e612bd89c1590dff5ac2ea51e5a09044863
root <yann-mael.bouton@epitech.eu>
2026-02-03 00:37:08 +0100

    chore(args): arguements parser mostly finished

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -1,30 +1,102 @@
-use clap::{Parser, SubCommand};
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(name = "anvl", version, about)]
 pub struct Args {
-    #[arg(short, long)]
+    #[arg(short = 'v', long)]
     pub verbose: bool,
-
-    #[arg(short = 'y', long)]
-    pub yes: bool,
 
     #[command(subcommand)]
     pub command: Command,
 }
 
-#[derive(SubCommand)]
+#[derive(Subcommand)]
 pub enum Command {
-    Init,
-    Install,
-    Update,
-    Remove,
+    Init {
+        #[command(subcommand)]
+        kind: InitKindCommand,
+        name: String,
+        #[arg(short = 'p', long)]
+        push: bool,
+    },
+    Install {
+        modules: Vec<String>,
+        #[arg(short = 'p', long)]
+        push: bool,
+    },
+    Update {
+        modules: Vec<String>,
+        #[arg(short = 'f', long)]
+        force: bool,
+        #[arg(short = 'p', long)]
+        push: bool,
+    },
+    Remove {
+        modules: Vec<String>,
+        #[arg(short = 'f', long)]
+        force: bool,
+        #[arg(short = 'p', long)]
+        push: bool,
+    },
     Status,
-    Doctor,
+    Doctor {
+        #[command(subcommand)]
+        cmd: DoctorCommand,
+    },
     List,
-    Info,
-    Cache,
-    Config,
-    Create,
-    Delete,
+    Info {
+        module: String,
+    },
+    Cache {
+        #[command(subcommand)]
+        cmd: CacheCommand,
+    },
+    Config {
+        #[command(subcommand)]
+        cmd: Option<ConfigCommand>,
+    },
+    Create {
+        #[command(subcommand)]
+        kind: CreateKindCommand,
+        path: String,
+        #[arg(short = 'p', long)]
+        push: bool,
+        #[arg(long = "no-test")]
+        no_test: bool,
+    },
+    Delete {
+        path: String,
+        #[arg(short = 'p', long)]
+        push: bool,
+    },
+}
+
+#[derive(Subcommand)]
+pub enum InitKindCommand {
+    Lib,
+    Bin,
+}
+
+#[derive(Subcommand)]
+pub enum CreateKindCommand {
+    C,
+    H,
+}
+
+#[derive(Subcommand)]
+pub enum DoctorCommand {
+    Template,
+    Project,
+}
+
+#[derive(Subcommand)]
+pub enum ConfigCommand {
+    Get,
+    Set { key: String, value: String },
+}
+
+#[derive(Subcommand)]
+pub enum CacheCommand {
+    Update,
+    Clean,
 }
