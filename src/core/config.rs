5cf852a014e83534f5575ef1ae8a03153766b38e
root <yann-mael.bouton@epitech.eu>
2026-02-03 22:01:58 +0100

    chore(config): add path resolution

--- src/core/config.rs
+++ src/core/config.rs
@@ -1,9 +1,23 @@
 use serde::{Deserialize, Serialize};
-use std::path::Path;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
     repo: String,
     anvl_storage_path: String,
     always_push: bool,
+}
+
+impl Config {
+    fn path() -> Result<PathBuf, String> {
+        let mut path = dirs::config_dir().ok_or("could not determine config directory")?;
+
+        path.push("anvl");
+        path.push("config.json");
+
+        Ok(path)
+    }
 }
